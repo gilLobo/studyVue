6b592407838c634e1c7601c6545c7d37c815a9c0
Gilberto Lobo <gilberto.lobo@Gilbertos-MacBook-Pro.local>
2021-01-09 22:24:04 -0300

    finalizando aulas propriedades reativas

---
+++ curso/dom/desafio-03/app.js
@@ -0,0 +1,18 @@
+new Vue({
+    el: '#desafio',
+    data: {
+        valor: 0,
+    },
+    watch: {
+        valor(antigo, novo) {
+            setTimeout(() => {
+                this.valor = 0
+            }, 5000)
+        },
+    },
+    computed: {
+        resultado() {
+            return this.valor != 37 ? 'Valor Diferente' : 'Valor Igual'
+        }
+    }
+});
