b2911c2682bb34130ed566ef737da770d29e9948
Gilberto Lobo <gilberto.lobo@Gilbertos-MacBook-Pro.local>
2021-01-16 10:46:23 -0300

    finalizando projeto matador de monstros

--- curso/projeto-01-mostro/app.js
+++ curso/projeto-01-mostro/app.js
@@ -1,9 +1,19 @@
 new Vue({
     el: '#app',
     data: {
+        // Controle execução do jogo
         running: false,
+        // Controle HP dos personagens
         playerLife: 100,
         monsterLife: 100,
+        // Controle de ataque e cura dos personagens
+        attackMinMonster: 7,
+        attackMaxMonster: 12,
+        attackMinPlayer: 5,
+        attackMaxPlayer: 10,
+        healMin: 10,
+        healMax: 15,
+        logs: []
     },
     computed: {
         hasResult() {
@@ -15,21 +25,36 @@ new Vue({
             this.playerLife = 100
             this.monsterLife = 100
             this.running = true
+            this.logs = []
         },
         attack(especial) {
-            this.hurt('playerLife', 7, 12, false)
-            this.hurt('monsterLife', 5, 10, false)
+            this.hurt('monsterLife', this.attackMinPlayer, this.attackMaxPlayer, especial, 'Jogador', 'Monstro', 'player')
+            if (this.monsterLife > 0) {
+                this.hurt('playerLife', this.attackMinMonster, this.attackMaxMonster, false, 'Monstro', 'Jogador', 'monster')
+            }
         },
-        hurt(atr, min, max, especial){
+        hurt(atr, min, max, especial, source, target, cls){
             const plus = especial ? 5 : 0
             const hurt = this.getRandom(min + plus, max + plus)
             this[atr] = Math.max(this[atr] - hurt, 0)
-            
+            this.registerLog(`${source} atigiu ${target} com ${hurt}.`, cls)
+        },
+        healAndHurt(){
+            this.heal(this.healMin, this.healMax)
+            this.hurt('playerLife', this.attackMinMonster, this.attackMaxMonster, false, 'Monstro', 'Jogador', 'monster')
+        },
+        heal(min, max) {
+            const heal = this.getRandom(min, max)
+            this.playerLife = Math.min(this.playerLife + heal, 100)
+            this.registerLog(`Jogador ganhou força de ${heal}.`, 'player')
         },
         getRandom(min, max) {
             const value = Math.random() * (max - min) + min
             return Math.round(value)
-        }
+        },
+        registerLog(text, cls) {
+            this.logs.unshift({ text, cls })
+        },
     },
     watch: {
         hasResult(value) {
